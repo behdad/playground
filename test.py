84d7f88361999e289c6980b63e3fe6dd27afe85e
Behdad Esfahbod <behdad@behdad.org>
2015-06-30 14:23:06 -0700

    Use pickle for caching
    Towards building GX...

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ import glyphs2ufo.torf
 #from ufo2fdk import OTFCompiler
 from fontbuild.convertCurves import glyphCurvesToQuadratic
 from fontbuild.outlineTTF import OutlineTTFCompiler
+from fontTools.ttLib import TTFont
 
-def build (src):
+def build_ttfs (src):
 
 	print "Loading Glyphs src `%s' into memory" % src
 	if not hasattr(src, 'read'):
@@ -17,8 +18,10 @@ def build (src):
 
 	print "Load into Robofab font"
 	masters = glyphs2ufo.torf.to_robofab(dic)
+	master_infos = dic['fontMaster']
 	del dic
 
+	master_ttfs = []
 	for master in masters:
 
 		family = master.info.familyName
@@ -38,8 +41,31 @@ def build (src):
 		print "Compiling master"
 		compiler = OutlineTTFCompiler(master, fullname+".ttf")
 		compiler.compile()
+		master_ttfs.append(fullname+".ttf")
+
+	return master_ttfs, master_infos
+
+def build_gx(master_ttfs, master_infos):
+	print "Building GX"
+	print "Loading TTF masters"
+	master_fonts = [TTFont(f) for f in master_ttfs]
+
 
 if __name__ == '__main__':
-	import sys
+	import sys, pickle
 	for src in sys.argv[1:]:
-		build (src)
+
+		pickle_file = src + '.pickle'
+		try:
+			c = pickle.load(open(pickle_file))
+		except (IOError, EOFError):
+			c = {}
+
+		if not 'master_ttfs' in c or not 'master_infos' in c:
+			c['master_ttfs'], c['master_infos'] = build_ttfs(src)
+
+		pickle.dump(c, open(pickle_file, 'wb'), pickle.HIGHEST_PROTOCOL)
+
+		if not 'gx' in c:
+			c['gx'] = build_gx(c['master_ttfs'], c['master_infos'])
+
