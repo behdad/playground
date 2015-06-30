661cf15b3ad26492a23a090dbd1d108d25d0ee1f
Behdad Esfahbod <behdad@behdad.org>
2015-06-30 13:42:31 -0700

    Build truetypes, using this patchset:
    https://github.com/google/roboto/pull/53

--- test.py
+++ test.py
@@ -1,16 +1,45 @@
 #!/usr/bin/python
 
 import glyphs2ufo.torf
-f = open("NotoSansUI-RomanMM.glyphs")
-
 import glyphs2ufo.glyphslib
-d = glyphs2ufo.glyphslib.load(f)
-
 import glyphs2ufo.torf
-masters = glyphs2ufo.torf.to_robofab(d)
+#from ufo2fdk import OTFCompiler
+from fontbuild.convertCurves import glyphCurvesToQuadratic
+from fontbuild.outlineTTF import OutlineTTFCompiler
 
-from ufo2fdk import OTFCompiler
-compiler = OTFCompiler() 
-reports = compiler.compile(masters[0], "a.otf", autohint=False)
-print reports["makeotf"]
+def build (src):
 
+	print "Loading Glyphs src `%s' into memory" % src
+	if not hasattr(src, 'read'):
+		src = open(src)
+	dic = glyphs2ufo.glyphslib.load(src)
+	del src
+
+	print "Load into Robofab font"
+	masters = glyphs2ufo.torf.to_robofab(dic)
+	del dic
+
+	for master in masters:
+
+		family = master.info.familyName
+		style = master.info.styleName
+		fullname = "%s-%s" % (family, style)
+
+		print "Processing master", fullname
+
+		#compiler = OTFCompiler()
+		#reports = compiler.compile(master, fullname+".otf", autohint=False)
+		#print reports
+
+		print "Converting outlines to quadratic"
+		for glyph in master:
+			     glyphCurvesToQuadratic(glyph)
+
+		print "Compiling master"
+		compiler = OutlineTTFCompiler(master, fullname+".ttf")
+		compiler.compile()
+
+if __name__ == '__main__':
+	import sys
+	for src in sys.argv[1:]:
+		build (src)
