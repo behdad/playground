eb53cd086ba507e847a9d0b5ab0e8147322de0fa
Behdad Esfahbod <behdad@behdad.org>
2015-10-01 20:01:07 +0100

    Print detected note

--- fft.py
+++ fft.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 
 import numpy as np
+import math
 
 f = open('/dev/stdin', 'rb')
 
 freq = 44100
-time = .08
+time = .1
+cutoff_freq = (1./time) * 2 * 4
 
 i = 0
 while True:
 	signal = list(f.read(int(freq * time)))
 	n = len(signal)
-	fft = abs(np.fft.fft(signal)[1:n/2] / (n / 2.))
-	note = fft.argmax() / time
+	fft = abs(np.fft.fft(signal)[:n/2] / (n / 2.))
 
-	print(note, '	', sep='', end='')
-	i += 1
-	if i % 8 == 0:
-		print()
+	# high-pass filtering.
+	fft[:1+(cutoff_freq * time)] = 0
+
+	# log-scale and quantize; throws away noise.
+	fft = np.floor(np.log(1+fft))
+
+	# skip silence.
+	if np.all(fft == 0):
+		continue
+
+	note_freq = int(fft.argmax() / time)
+	note = round(math.log(note_freq / 440., 2**(1/12.)))
+	octave = 4 + int(note / 12)
+	note = note % 12
+	note = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'][note]
+	note += str(octave)
+
+	print(note, '%4d'%note_freq, ''.join('%1X' % x for x in fft[:200]))
